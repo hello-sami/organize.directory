aeaf9b23c89c37b736409293abdb11e99be45d7e
hello-sami <sami@samismith.com>
2025-02-18 16:56:57 -0500

    error fixes

--- js/analytics.js
+++ js/analytics.js
@@ -1,10 +1,17 @@
-// Google Analytics
+// Google Analytics with respect for privacy
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-QJ03RCM35H');
 
-// Load GA script
+// Initialize GA with restricted data collection
+gtag('js', new Date());
+gtag('config', 'G-QJ03RCM35H', {
+    'cookie_flags': 'SameSite=None;Secure',
+    'anonymize_ip': true,
+    'allow_google_signals': false,
+    'allow_ad_personalization_signals': false
+});
+
+// Load GA script with privacy-first settings
 const gaScript = document.createElement('script');
 gaScript.async = true;
 gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-QJ03RCM35H';
