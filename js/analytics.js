5947a55dac7228e5350cd236ca216def01bc0e0e
hello-sami <sami@samismith.com>
2025-02-18 16:49:56 -0500

    performance fixes

---
+++ js/analytics.js
@@ -0,0 +1,11 @@
+// Google Analytics
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-QJ03RCM35H');
+
+// Load GA script
+const gaScript = document.createElement('script');
+gaScript.async = true;
+gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-QJ03RCM35H';
+document.head.appendChild(gaScript); 
