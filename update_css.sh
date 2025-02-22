29d7a0085d67f521f3c79dba1b5bddac84a78151
hello-sami <sami@samismith.com>
2025-02-22 12:12:28 -0500

    adding in link styles

--- update_css.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-find . -type f -name "*.html" -exec sed -i "" "/<link.*href=\"\/styles\/city\.css\".*>/d" {} +
-find . -type f -name "*.html" -exec sed -i "" "/<link.*href=\"\/styles\/location\.css\".*>/d" {} +
-find . -type f -name "*.html" -exec sed -i "" "/<link.*href=\"\/styles\/states-list\.css\".*>/d" {} +
-echo "CSS references removed successfully!"
